957791c324101b908c3b3a6f7e67a880550b7702
Lyline <greg.quesne@gmail.com>
2022-03-28 10:14:39 +0200

    initial commit

---
+++ src/main/java/rewardcentralmicroservice/RewardCentralMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package rewardcentralmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RewardCentralMicroserviceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(RewardCentralMicroserviceApplication.class, args);
+  }
+
+}
---
+++ src/test/java/tourguide/microservice/rewardcentralmicroservice/RewardCentralMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package tourguide.microservice.rewardcentralmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RewardCentralMicroserviceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
